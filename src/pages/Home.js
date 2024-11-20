d45ce3c30b702b4731a8e2308722599149ccc321
Muhammad Rehman <141804487+MRehman365@users.noreply.github.com>
2024-11-20 10:39:40 -0800

    aa

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,11 +2,7 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div>
-      <h1 className="text-black twxt-2xl">
-        Home page Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Officia, fuga.
-      </h1>
+    <div className="bg-[#2b2b2b] min-h-screen">
     </div>
   );
 };
